810a9913c71003f26d0b801e5acec2b3100ae9c7
Irina <zaletk.irina@gmail.com>
2019-10-01 17:44:31 +0300

    refactor: delete duplicate variables

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ let decode =expr=>{
 const space = "**********";
 let countLetters = expr.length/10;
 let results = '';
-let j =0;
+
 for (j =0 ;j<countLetters;j++){
     let letter =expr.substr(10*j,10);
     results += (letter === space) ?' ' : getLetters(letter);
@@ -50,7 +50,6 @@ return results;
 }
 let getLetters = expr=> {
     let results = "";
-    let i = 0;
     for (let i=0;i < 5;i++) {
         let val = expr.substr(2*i, 2);
         results += (val === "00") ? '' : val === "10" ? '.' : '-';
